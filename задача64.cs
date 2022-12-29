ec81bc4c382e4c4f31a1b23acca198ab80a0be1d
kokothina <119644933+kokothina@users.noreply.github.com>
2022-12-29 18:39:39 +1000

    Add files via upload

--- задача64.cs
+++ задача64.cs
@@ -1,14 +1,30 @@
 using System;
 class HelloWorld {
   static void Main() {
-    int N = Convert.ToInt32(Console.ReadLine());
-    rec(N);
-  }
-  static void rec(int N)
-  {
-      Console.WriteLine(N);
-      N--;
-      if (N>=1)
-      rec(N);
+Console.WriteLine("Сколько элементов массива вы собираетесь ввести?");
+int n;
+n = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите элементы массива:");
+string [] mas = new string[n];
+string [] new_mas = new string[n];
+for (int i=0;i<n;i++)
+{
+    mas[i] = Console.ReadLine();
+    
+}
+int j = 0;
+for (int i=0;i<n;i++)
+{
+    if (mas[i].Length < 4)
+    {
+        new_mas[j] = mas[i];
+        j++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Новый сформированный массив");
+Console.WriteLine();
+for (int i=0;i<j;i++)
+Console.WriteLine(new_mas[i]);
   }
 }
